e5f9f6eb97b1d0f803e4293f9e343a66e4a0b594
Nezouse <stankiewicz1998@gmail.com>
2019-04-09 22:56:14 +0200

    feat: Enforce model validation in list view

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 3
+MAX_WAIT = 10
 
 
 class FunctionalTest(StaticLiveServerTestCase):
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -15,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has_error').text,
+            self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         ))
 
