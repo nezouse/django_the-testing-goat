0fa4395773138dc6ff0813e15da4c707fc401431
Nezouse <stankiewicz1998@gmail.com>
2019-04-20 19:47:47 +0200

    Change validation for forms and models

--- lists/forms.py
+++ lists/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 
 class ItemForm(forms.models.ModelForm):
@@ -23,3 +25,9 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
+
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
