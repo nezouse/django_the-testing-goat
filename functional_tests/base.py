1f6962fd5b0ec1ac19ceff4d38c84238dd8f2321
Nezouse <stankiewicz1998@gmail.com>
2019-05-18 10:44:00 +0200

    Create Page objects for pages, use in sharing FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -13,6 +13,20 @@ from .server_tools import create_session_on_server, reset_database
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -23,19 +37,6 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
